2750aa2d720bbbf858c5f5207270cadc9d820ed3
Vishal Nakum <vishalhnakum011@gmail.com>
2019-09-21 05:19:20 +0530

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => (
+    <React.Fragment>
+        <div style={{
+            textAlign: 'center',
+            padding: 40
+        }}>
+            <small>Made with 💜by <a href="https://twitter.com/amoltangade" style={{ textDecoration: "none" }}>snowman.</a>This site is <a href="https://github.com/vishalnakum011/contentful">open source.</a></small>
+        </div>
+    </React.Fragment>
+)
