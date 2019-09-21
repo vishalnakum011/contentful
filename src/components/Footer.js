5f0d8bbe8907f621c52a7bb6a6c34b4e8b23fd56
Vishal Nakum <vishalhnakum011@gmail.com>
2019-09-21 06:06:59 +0530

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default () => (
             textAlign: 'center',
             padding: 40
         }}>
-            <small>Made with 💜by <a href="https://twitter.com/amoltangade" style={{ textDecoration: "none" }}>snowman.</a>This site is <a href="https://github.com/vishalnakum011/contentful">open source.</a></small>
+            <small>Made with 💜 by <a href="https://twitter.com/amoltangade" style={{ textDecoration: "none" }}>snowman.</a>This site is <a href="https://github.com/vishalnakum011/contentful">open source.</a></small>
         </div>
     </React.Fragment>
 )
